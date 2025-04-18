85aa8831c3be0356ed0ba15b8b7658a1f40121dc
mclaraoliveira <clara.santoss.oliveira@gmail.com>
2025-04-17 21:11:33 -0300

    FEAT: Criar rotas da página para acessar mural e formulário.

--- projetos/questao-11-memoteca/src/app/app.routes.ts
+++ projetos/questao-11-memoteca/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PaginaMuralComponent } from './pages/pagina-mural/pagina-mural.component';
+import { PaginaFormularioComponent } from './pages/pagina-formulario/pagina-formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PaginaMuralComponent },
+  { path: 'mural', component: PaginaMuralComponent },
+  { path: 'formulario', component: PaginaFormularioComponent },
+  { path: '**', redirectTo: '/mural' } // Redireciona para mural se a rota não existir
+];
